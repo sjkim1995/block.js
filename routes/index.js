4f316c792b0b52d130c53ecd2286bab453ccd70a
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-12-03 21:04:13 -0500

    sockets in progress

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 module.exports = app => {
 
+
+	// middleware for checking if miner is still running
+  app.use('/', (req, res, next) => {
+  	const io = app.get('socketio');
+  	next();
+  })
+
   app.get('/', (req, res) => {
     res.render('../views/index');
+  });
+
+  app.get('/boilerplate', (req, res) => {
+  	res.render('../views/boilerplate');
   });
 
 };
