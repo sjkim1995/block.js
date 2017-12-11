1bf6714fc5d9018af0bb55abfc62fa7d47384364
conorquinn <conor@kensho.com>
2017-12-11 15:46:21 -0500

    removed old dependencies

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const app = express();
 
 consign({ verbose: false })
   .include('libs/config.js')
-  .then('db.js')
-  .then('auth.js')
   .then('libs/middlewares.js')
   .then('routes')
   .then('libs/boot.js')
--- libs/middlewares.js
+++ libs/middlewares.js
@@ -26,7 +26,6 @@ module.exports = app => {
     allowedHeaders: ['Content-Type', 'Authorization'],
   }));
   app.use(bodyParser.json());
-  app.use(app.auth.initialize());
   app.use((req, res, next) => {
     delete req.body.id;
     next();
